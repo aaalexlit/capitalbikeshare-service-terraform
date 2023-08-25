8b7f680b23e2a004727ec559bdc383b72d895ba9
Alex Litvinov <aaalex.lit@gmail.com>
2023-08-25 22:13:31 +0200

    fix container name output

--- outputs.tf
+++ outputs.tf
@@ -18,7 +18,7 @@ output "task_definition_json" {
 }
 
 output "container_name" {
-  value = aws_ecs_task_definition.service_task.container_definitions.name
+  value = jsonencode(aws_ecs_task_definition.service_task.container_definitions).name
 }
 
 output "ecs_service" {
